628976a3b2971a788a04ae6d064b5bb66e6f2cb4
Jason1926 <tenwa1926@gmail.com>
2016-01-17 21:24:50 +0800

    第 四個Scenario亮紅燈

--- day2-homework/day2-homeworkTests/BookShoppingCartTest.cs
+++ day2-homework/day2-homeworkTests/BookShoppingCartTest.cs
@@ -46,12 +46,31 @@ namespace day2_homeworkTests
             var products = new List<Product>() { 
                                                  new Product() { Episode = "1" , Price = 100 } ,
                                                  new Product() { Episode = "2" , Price = 100 } ,
-                                                 new Product() { Episode = "3" , Price = 100 }
+                                                 new Product() { Episode = "3" , Price = 100 } ,
                                                };
 
             var actual = ShoppingCart.CheckOut(products);
 
             int expected = 270;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_BookShoppingCart_is_1Episode_1_AND_2Episode_1_And_3Episode_1_AND_4Episode_1_Buy_by_TotalPrice_Should_Be_320()
+        {
+            var target = new ShoppingCart();
+
+            var products = new List<Product>() { 
+                                                 new Product() { Episode = "1" , Price = 100 } ,
+                                                 new Product() { Episode = "2" , Price = 100 } ,
+                                                 new Product() { Episode = "3" , Price = 100 } ,
+                                                 new Product() { Episode = "4" , Price = 100 }
+                                               };
+
+            var actual = ShoppingCart.CheckOut(products);
+
+            int expected = 320;
 
             Assert.AreEqual(expected, actual);
         }
